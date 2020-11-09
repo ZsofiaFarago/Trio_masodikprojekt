830d14ceaba0732e49af40e33f00ca87af4cec34
Faragó Zsófia <farago.zsofia.1995@gmail.com>
2020-11-09 18:37:11 +0100

    A Kártevőirtás oldal HTML struktúrájának kialakítása

--- Projekt/service_project/protected/services/pest_control/start.php
+++ Projekt/service_project/protected/services/pest_control/start.php
@@ -1,1 +1,29 @@
-Kártevőirtás
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta content="charset=UTF-8">
+		<script src="<?=PUBLIC_DIR.'Js/pest_control_menu_highlight.js'?>"></script>
+		<link rel="stylesheet" href="<?=PUBLIC_DIR.'css/pest_control_style.css'?>">
+		<title>Kártevőirtás</title>
+	</head>
+	<body onload="selectMenu()">
+		<div id="header">
+			<img id="logo" src="<?=PUBLIC_DIR.'Images/pest_control_logo.png'?>" />
+			<h1 id="title">Kártevőirtás</h1>
+		</div>
+		<div id="nav">
+			<div id="menu">
+				<a class="menupont" id="main_menu" href="index.php?S=pest_control&A=home">Főoldal</a>
+				<a class="menupont" id="pest_menu" href="index.php?S=pest_control&A=pest">Kártevők</a>
+				<a class="menupont" id="order_menu" href="index.php?S=pest_control&A=order">Megrendelés</a>
+				<a class="menupont" id="registration_menu" href="index.php?S=pest_control&A=registration">Regisztráció</a>
+			</div>
+		</div>
+		<div class="main">
+			<?php require_once PROTECTED_DIR.'services/pest_control/routing.php'; ?>
+		</div>
+		<div id="footer">
+			<p>Copyright &copy; 2020 AFP1 Trio. All Rights Reserved</p>
+		</div>
+	</body>
+</html>
