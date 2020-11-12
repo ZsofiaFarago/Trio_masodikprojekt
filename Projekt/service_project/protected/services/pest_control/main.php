cb9ae52a6631685dd9624663a6167b08db049f94
Faragó Zsófia <farago.zsofia.1995@gmail.com>
2020-11-12 23:39:09 +0100

    A kártevőirtás főoldalán az árajánlat kialakítása

--- Projekt/service_project/protected/services/pest_control/main.php
+++ Projekt/service_project/protected/services/pest_control/main.php
@@ -1,1 +1,26 @@
-<h1>Főoldal</h1>
+<?php
+	require_once 'database.php';
+	$queryString = "SELECT name, base_price FROM pest;";
+	$records = getList($queryString);
+?>
+<h1>Árajánlat</h1>
+<table class="container">
+	<thead>
+		<tr>
+			<th>Kártevő</th>
+			<th>Alapdíj</th>
+		</tr>
+	</thead>
+	<tbody>
+        <?php foreach ($records as $record) :?>
+        <tr>
+            <td><?=$record['name'] ?></td>
+            <td><?=$record['base_price'] ?> Ft</td>
+        </tr>
+        <?php endforeach; ?>
+	</tbody>
+	<tfoot>
+		<td>Kiszállási díj</td>
+		<td>5000 Ft</td>
+	</tfoot>
+</table>
