77260f1484fa72b7e91246b446f249ddfd200784
Faragó Zsófia <farago.zsofia.1995@gmail.com>
2020-11-12 22:38:28 +0100

    Konfiguráció beállítása az kártevőirtás adatbázisához, régi törlése

---
+++ Projekt/service_project/protected/services/pest_control/config.php
@@ -0,0 +1,8 @@
+<?php
+	define('DB_TYPE','mysql');
+	define('DB_HOST','bfjhcumb7nyuxaryu8ah-mysql.services.clever-cloud.com');
+	define('DB_NAME','bfjhcumb7nyuxaryu8ah');
+	define('DB_USER','uhuet7u7oavwnwte');
+	define('DB_PASS','W6lcVQsGyqhXjkL8X4wE');
+	define('DB_CHARSET', 'utf8');
+?>
