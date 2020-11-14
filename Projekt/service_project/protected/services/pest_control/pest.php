cb35ba4ba9cd9f3663a4c82bcb413635be32e949
Faragó Zsófia <farago.zsofia.1995@gmail.com>
2020-11-14 13:41:36 +0100

    Kártevőirtás: kártevők információs lapjának kialakítása, adatok lekérése

--- Projekt/service_project/protected/services/pest_control/pest.php
+++ Projekt/service_project/protected/services/pest_control/pest.php
@@ -1,1 +1,15 @@
+<?php
+	require_once 'database.php';
+	$queryString = "SELECT name, type, danger_level, description, image FROM pest;";
+	$records = getList($queryString);
+?>
 <h1>Kártevők</h1>
+<?php foreach ($records as $record) :?>
+<div class="info">
+	<img class="index" src="<?=PUBLIC_DIR.'Images/'.$record['image']?>" />
+	<h2><?=$record['name']?></h2>
+	<p>Típusa: <?=$record['type']?></p>
+	<p>Veszélyességi szint: 10/<?=$record['danger_level']?></p>
+	<p><?=$record['description']?></p>
+</div>
+<?php endforeach; ?>
