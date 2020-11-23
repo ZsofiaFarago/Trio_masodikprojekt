57dcdb8830b67c4ecf7dfd9f9240a19e66d56e1a
Faragó Zsófia <farago.zsofia.1995@gmail.com>
2020-11-24 00:46:05 +0100

    Javítás az Információk menüpontban

--- Projekt/service_project/protected/information.php
+++ Projekt/service_project/protected/information.php
@@ -1,7 +1,5 @@
 <?php require_once PROTECTED_DIR.'header.php'; ?>
 		<div class="main">
-<?php require_once PROTECTED_DIR.'header.php'; ?>
-		<div class="main">
 			<div class="info">
 				<img class="index" src="<?=PUBLIC_DIR.'Images/posta.jpeg'?>" />
 				<h2>Magyar Posta</h2>
@@ -9,16 +7,10 @@
 				<a class="link" href="https://www.posta.hu/fooldal">Tovább >></a>
 			</div>
 			<div class="info">
-			</div>
-			<div class="info">
 				<img class="index" src="<?=PUBLIC_DIR.'Images/idojaras.jpeg'?>" />
 				<h2>Időjárás</h2>
 				<p>A köpönyeg.hu egy időjárás-előrejelző honlap, mely a New Wave Media Group gondozásában jelenik meg. A köpönyeg.hu olvasói között az amatőr időjósok mellett megtalálhatók a szabadtéri sportok kedvelői – például motorosok, vitorlázók, golfozók –, és azok is, akik valamilyen szabadtéri programot terveznek. Vannak, akik csak azt szeretnék tudni, milyen ruhát érdemes venniük, és vigyenek-e magukkal esernyőt vagy éppen köpönyeget.</p>
 				<a class="link" href="https://koponyeg.hu/elorejelzes/">Tovább >></a>
 			</div>
-			<div class="info">
-			</div>
-		</div>
-<?php include_once PROTECTED_DIR.'footer.php'; ?>
 		</div>
 <?php include_once PROTECTED_DIR.'footer.php'; ?>
