92d883ce275d61b6c2d6de099e100a7427e98c99
Spokieh <dohanyos96@gmail.com>
2020-11-23 16:46:46 +0100

    Sport: Kezdooldal Contact Us form letrehozasa + PHP hozza

--- Projekt/service_project/protected/services/training/main.php
+++ Projekt/service_project/protected/services/training/main.php
@@ -194,3 +194,39 @@
 
 <div class="col-md-12 gap10"></div>
 </div>
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Contact Us</h2>
+
+    <div class="w-50 m-auto">
+      <form action="userinfo.php" method="post">
+        <div class="form-group">
+          <label> Username </label>
+          <input type="text" name="user" autocomplete="off" class="form-control">
+        </div>
+        <div class="form-group">
+          <label> E-mail </label>
+          <input type="text" name="email" autocomplete="off" class="form-control">
+        </div>
+        <div class="form-group">
+          <label> Phone </label>
+          <input type="text" name="mobile" autocomplete="off" class="form-control">
+          <div class="form-group">
+            <label> Comments </label>
+            <textarea class="form-control" name="">
+              </textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+
+      </form>
+    </div>
+</section>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
