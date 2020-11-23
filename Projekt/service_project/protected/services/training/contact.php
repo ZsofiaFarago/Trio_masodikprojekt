6deb0b637fa2c96a73cae101582c6c51acc3bfbc
Spokieh <dohanyos96@gmail.com>
2020-11-23 16:26:32 +0100

    Sport: Contact ful kitoltese

---
+++ Projekt/service_project/protected/services/training/contact.php
@@ -0,0 +1,51 @@
+<section id="contact">
+    <div class="container">
+        <br>
+        <h1 class="text-center text-uppercase">contact us</h1>
+        <p class="text-center w-75 m-auto">You can reach us here if you have any question where to start, or want your company to be promoted here and any other things.</p>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-3 my-5">
+                <div class="card border-0">
+                    <div class="card-body text-center">
+                        <i class="fa fa-phone fa-5x mb-3" aria-hidden="true"></i>
+                        <h4 class="text-uppercase mb-5">call us</h4>
+                        <p>+36301234567,+36010123456</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 my-5">
+                <div class="card border-0">
+                    <div class="card-body text-center">
+                        <i class="fa fa-language fa-5x mb-3" aria-hidden="true"></i>
+                        <h4 class="text-uppercase mb-5">Laungages</h4>
+                        <address>English, Hungarian </address>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 my-5">
+                <div class="card border-0">
+                    <div class="card-body text-center">
+                        <i class="fa fa-home fa-5x mb-3" aria-hidden="true"></i>
+                        <h4 class="text-uppercase mb-5">Owner</h4>
+                        <address>TRIO </address>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 my-5">
+                <div class="card border-0">
+                    <div class="card-body text-center">
+                        <i class="fa fa-globe fa-5x mb-3" aria-hidden="true"></i>
+                        <h4 class="text-uppercase mb-5">email</h4>
+                        <p><a href="mailto:info@stayfit.com">info@stayfit.com</a> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+
