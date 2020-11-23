da1f7ade1a8c7d7e55c83ad355702420196c5abe
Spokieh <dohanyos96@gmail.com>
2020-11-23 05:15:36 +0100

    Sport: Az oldalak felepitesenek kidolgozasa

---
+++ Projekt/service_project/protected/services/training/main.php
@@ -0,0 +1,29 @@
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+<ul class="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  <li data-target="#demo" data-slide-to="2"></li>
+</ul>
+
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" alt="Never" width="1100" height="500">
+  </div>
+  <div class="carousel-item">
+    <img src="<?=PUBLIC_DIR.'Images/cc2.jpg'?>" alt="Give" width="1100" height="500">
+  </div>
+  <div class="carousel-item">
+    <img src="<?=PUBLIC_DIR.'Images/cc3.jpg'?>" alt="Up" width="1100" height="500">
+  </div>
+</div>
+
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</a>
+</div>
+
--- Projekt/service_project/protected/services/training/start.php
+++ Projekt/service_project/protected/services/training/start.php
@@ -4,31 +4,52 @@
 		<meta content="charset=UTF-8">
 		<link rel="stylesheet" href="<?=PUBLIC_DIR.'css/sportStyle.css'?>">
 		<title>StayFit</title>
+		    
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+
+       
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://c.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	</head>
-	<body>
+	<body onload="selectMenu()">
+		
+		
+		
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="index.php?S=training&A=home">StayFit</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div>
-        <ul>
-            <li>
-                <a href="index.php?S=training&A=home">Home </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?S=training&A=home">Home </a>
             </li>
-            <li>
-                <a href="#">Services</a>
+            <!--
+            <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
             </li>
-            <li>
-                <a href="index.php?S=training&A=partners">Partners</a>
+            //-->
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?S=training&A=partners">Partners</a>
             </li>
-            <li>
-                <a href="index.php?S=training&A=contact">Contact</a>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?S=training&A=contact">Contact</a>
         </ul>
-        <form>
-            <input type="search" placeholder="Search" aria-label="Search">
-            <button type="submit">Search</button>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
 </nav>
