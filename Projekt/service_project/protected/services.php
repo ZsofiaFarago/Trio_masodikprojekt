19cd3b1d465c3e3ac1a436bd0daa00fa6549d635
Marci <pmarcell50@gmail.com>
2020-11-23 22:38:58 +0100

    Car rent menü hozzáadása a szolgáltatásokhoz

--- Projekt/service_project/protected/services.php
+++ Projekt/service_project/protected/services.php
@@ -12,5 +12,11 @@
 				<p>Vállalkozásunkat több mint 10 éves szakmai tapasztalat alapján hoztuk létre, hogy kitűnjünk elhivatottságunkkal és minőségi munkavégzésünkkel. Célunk az, hogy mind személyünket, mind munkánkat elismerjék és ehhez eddig is mindent megtettünk, ahogyan ezután is tenni fogjuk! Hírünk szájról szájra is terjed, és ez nem véletlen, ennél jobb reklám nem is kell. Professzionális irtószereink, emberbarát kommunikációnk, ügyfélorientáltságunk és versenyképes áraink sikerünk kulcsai, melyeken a jövőben sem kívánunk változtatni!</p>
 				<a class="link" href="index.php?S=pest_control">Tovább >></a>
 			</div>
+			<div class="info">
+				<img class="index" src="<?=PUBLIC_DIR.'Images/car_rent.png'?>" width="200" height="120" />
+				<h2>Autóbérlés</h2>
+				<p>Béreljen személygépjárművet, vagy kistehergépjárművet az ország több pontjáról, otthonról, könnyedén. Telephelyünk minden árkategóriában tartalmaz gépjárművet, így mindenki megtalálja az igényeihez illőt. Kauciót csak az 5 évnél fiatalabb gépjárműveinkre szükséges fizetni! Vegye igénybe szolgáltatásunkat most!</p>
+				<a class="link" href="index.php?S=car_rent">Tovább >></a>
+			</div>
 		</div>
 <?php include_once PROTECTED_DIR.'footer.php'; ?>
