793c94e4260569aae6761de4dfcb43ec2b378bab
Faragó Zsófia <farago.zsofia.1995@gmail.com>
2020-11-24 00:31:02 +0100

    Az információs menüpont tartalmának feltöltése

--- Projekt/service_project/protected/information.php
+++ Projekt/service_project/protected/information.php
@@ -1,8 +1,24 @@
 <?php require_once PROTECTED_DIR.'header.php'; ?>
 		<div class="main">
+<?php require_once PROTECTED_DIR.'header.php'; ?>
+		<div class="main">
 			<div class="info">
+				<img class="index" src="<?=PUBLIC_DIR.'Images/posta.jpeg'?>" />
+				<h2>Magyar Posta</h2>
+				<p>A Magyar Posta Zrt. állami tulajdonban lévő vállalat, mely Magyarország postaforgalmát bonyolítja le. A Magyar Posta a levél- és csomagküldemények felvétele mellett pénzügyi és pénzforgalmi szolgáltatásokat (befizetés, kifizetés, pénzküldemény stb.) is nyújt, illetve hírlapterjesztést és kiskereskedelmi tevékenységet is végez (boríték, bélyeg, képeslap stb.értékesítése). A vállalat ezenfelül marketing szolgáltatásokat, logisztikai szolgáltatásokat, folyószámla- és hitelszolgáltatásokat és biztosítást is kínál.</p>
+				<a class="link" href="https://www.posta.hu/fooldal">Tovább >></a>
 			</div>
 			<div class="info">
 			</div>
+			<div class="info">
+				<img class="index" src="<?=PUBLIC_DIR.'Images/idojaras.jpeg'?>" />
+				<h2>Időjárás</h2>
+				<p>A köpönyeg.hu egy időjárás-előrejelző honlap, mely a New Wave Media Group gondozásában jelenik meg. A köpönyeg.hu olvasói között az amatőr időjósok mellett megtalálhatók a szabadtéri sportok kedvelői – például motorosok, vitorlázók, golfozók –, és azok is, akik valamilyen szabadtéri programot terveznek. Vannak, akik csak azt szeretnék tudni, milyen ruhát érdemes venniük, és vigyenek-e magukkal esernyőt vagy éppen köpönyeget.</p>
+				<a class="link" href="https://koponyeg.hu/elorejelzes/">Tovább >></a>
+			</div>
+			<div class="info">
+			</div>
+		</div>
+<?php include_once PROTECTED_DIR.'footer.php'; ?>
 		</div>
 <?php include_once PROTECTED_DIR.'footer.php'; ?>
