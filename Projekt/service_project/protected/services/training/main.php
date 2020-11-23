e7cfa5967b1bb31e1d4de2a3737a99b5f7dadbe0
Spokieh <dohanyos96@gmail.com>
2020-11-23 05:39:17 +0100

    Sport: Kezdooldal kitoltese

--- Projekt/service_project/protected/services/training/main.php
+++ Projekt/service_project/protected/services/training/main.php
@@ -27,3 +27,170 @@
 </a>
 </div>
 
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">What is your BMI?</h2>
+  </div>
+  <div class="conttainer-fluid">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-12">
+        <img src="<?=PUBLIC_DIR.'Images/cc4.jpg'?>" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12">
+        <h2 class="display-4">BMI calculator</h2>
+        <p class="py-3">Use this calculator to check your body mass index (BMI) and find out if you're a healthy weight. Or you can use it to check your child's BMI.</p>
+        <a href="#" class="btn btn-success"> To check it </a>
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Trusted shops for the best prices</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="<?=PUBLIC_DIR.'Images/buildershop.jpg'?>" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Buildershop</h4>
+            <p class="card-text">They are a huge blog, shop in Hungary </p>
+            <a href="https://shopbuilder.hu/" target="_blank" class="btn btn-primary">Visit them</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="<?=PUBLIC_DIR.'Images/5nutrition.jpg'?>" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">5% Nutrition</h4>
+            <p class="card-text">Founded by the legendary bodybuilder, Rich Piana, to deliver the most hardcore supplement formulas while never sacrificing taste. For all who are willing to do Whatever It Takes to achieve their goals, we are the 5% Family.</p>
+            <a href="https://5percentnutrition.com/" target="_blank" class="btn btn-primary">Visit them</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="<?=PUBLIC_DIR.'Images/biotech.jpg'?>" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">BioTechUSA</h4>
+            <p class="card-text">One of the most known suplements seller company.</p>
+            <a href="https://shop.biotechusa.hu/" target="_blank" class="btn btn-primary">Visit them</a>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
+
+</section>
+
+
+<!--
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Our Services</h2>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="<?=PUBLIC_DIR.'Images/cc1.jpg'?>" class="img-fluid pb-4">
+        </div>
+
+      </div>
+    </div>
+
+</section>
+//-->
+
+<h1 class="text-center">Should you use supplements?</h1>
+
+
+<div class="col-md-10 blogShort">
+  <h2>Creatine</h2>
+  <img src="<?=PUBLIC_DIR.'Images/creatine.png'?>" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail" style="width:150px">
+
+  <article>
+    <h3>What Is Creatine?</h3>
+    <p>
+      Creatine helps you recover between sets. Which means a supplements’ value lies in boosting recovery speed, which in turn enhances the amount of work you’re able to do during a workout. Over time, this leads to faster gains in both strength and size.
+      Creatine has proven itself over the years to be one of the most effective supplements for improving performance during repeated bouts of intense exercise. As far back as the 1970s, Soviet scientists knew that creatine supplements improved performance, and it contributed to the USSR’s Olympic dominance through the 70s and 80s.</br></br></br>
+    </p>
+  </article>
+  <a href="https://www.healthline.com/nutrition/what-is-creatine" target="_blank" class="btn btn-primary">Learn more about it</a>
+
+</div>
+</br>
+<div class="col-md-10 blogShort">
+  <h2>Whey Protein</h2>
+  <img src="<?=PUBLIC_DIR.'Images/wheyprotein.png'?>" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail" style="width:150px">
+
+  <article>
+    <h3>What Is Whey Protein?</h3>
+    <p>
+      Whey protein is a mixture of proteins isolated from whey, which is the liquid part of milk that separates during cheese production.
+
+      Milk actually contains two main types of protein: casein (80%) and whey (20%).
+
+      Whey is found in the watery portion of milk. When cheese is produced, the fatty parts of the milk coagulate and the whey is separated from it as a byproduct.
+
+      If you’ve ever opened a yogurt container to see liquid floating on top — that’s whey. Cheesemakers used to discard it before they discovered its commercial value
+    </p>
+  </article>
+  <a href="https://www.healthline.com/nutrition/whey-protein-101" target="_blank" class="btn btn-primary">Learn more about it</a>
+
+</div>
+</br>
+<div class="col-md-10 blogShort">
+  <h2>Multivitamins</h2>
+  <img src="<?=PUBLIC_DIR.'Images/multivit.png'?>" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail" style="width:150px">
+
+  <article>
+    <h3>What are multivitamins?</h3>
+    <p>
+      Multivitamins are supplements that contain many different vitamins and minerals, sometimes alongside other ingredients (3).
+
+      As there’s no standard for what constitutes a multivitamin, their nutrient composition varies by brand and product.
+
+      Multivitamins are also called multiminerals, multis, multiples, or simply vitamins.
+
+      They’re available in many forms, including tablets, capsules, chewable gummies, powders, and liquids.
+
+      Most multivitamins should be taken once or twice a day. Make sure to read the label and follow the recommended dosage instructions.</br></br></br>
+    </p>
+  </article>
+  <a href="https://www.healthline.com/nutrition/do-multivitamins-work" target="_blank" class="btn btn-primary">Learn more about it</a>
+
+</div>
+
+<div class="col-md-12 gap10"></div>
+</div>
