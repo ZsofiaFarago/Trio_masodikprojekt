e7a20970d6b2351f98c0dbbb39cb83db5a5955a3
Spokieh <dohanyos96@gmail.com>
2020-11-23 16:19:36 +0100

    Sport: Partners oldal feltoltese kontentel

---
+++ Projekt/service_project/protected/services/training/partners.php
@@ -0,0 +1,73 @@
+
+<div class="jumbotron">
+    <h1>Sites you should visit</h1>
+    <p>YouTube channels, blogs.</p>
+</div>
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center">Our Partners</h2>
+    </div>
+    <div class="conttainer-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+                <img src="<?=PUBLIC_DIR.'Images/AX-logo.png'?>" class="img-fluid aboutimg">
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+                <h2 class="display-4">Athlean-x</h2>
+                <p class="py-3">
+                    <h4>THE ATHLEAN-X CREATOR JEFF CAVALIERE</h4>
+                    Jeff Cavaliere is a fitness guru, social media star, personal trainer, and previous head physical therapist of the New York Mets ( professional baseball team).
+                    He became fascinated with bodybuilding after seeing Arnold Schwarzenegger and action movie stars as a child. As a result, he chose to study physioneurobiology and physical therapy at college – in order to pursue a career in the fitness industry.
+                    Since then, Jeff has become one of the most recognizable online fitness sensations – known for the training system he developed after leaving his position with the New York Mets.
+                </p>
+                <a href="https://www.youtube.com/user/JDCav24" target="_blank" class="btn btn-success"> To know more </a>
+
+            </div>
+        </div>
+    </div>
+
+    <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+    <br>
+
+    <div class="conttainer-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+			
+                <img src="<?=PUBLIC_DIR.'Images/fitnessfaqs.png'?>" class="img-fluid aboutimg">
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+                <h2 class="display-4">FITNESSFAQs</h2>
+                <p class="py-3">
+                    Daniel Vadnal is an international calisthenics expert with more than 10 years experience. He is renowned for his ability to educate, empower and motivate. Daniel is in high demand as a teacher, having travelled the world hosting sold out workshops throughout Australia, Asia and Europe.
+                </p>
+                <a href="https://www.youtube.com/user/FitnessFAQs" target="_blank" class="btn btn-success"> To know more </a>
+
+            </div>
+        </div>
+    </div>
+
+    <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+    <br>
+
+    <div class="conttainer-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+                <img src="<?=PUBLIC_DIR.'Images/greg.png'?>" class="img-fluid aboutimg">
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+                <h2 class="display-4">Greg Doucette</h2>
+                <p class="py-3">
+                    He is a Canadian IFBB Pro bodybuilder, published author, influencer, and Guinness World Record holding powerlifter with a Bachelors and Masters Degrees in Kinesiology. He coaches thousands of diet, training, and supplement clients around the world and get them to their goals in a sustainable way that fits their lifestyle. He tells you the TRUTH, no BS!
+                </p>
+                <a href="https://www.youtube.com/user/gregdoucette" target="_blank" class="btn btn-success"> To know more </a>
+
+            </div>
+        </div>
+    </div>
+
+
+
+</section>
+
+
