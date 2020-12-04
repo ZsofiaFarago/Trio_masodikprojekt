018f0f08caae622d7255883cff19475495e8b9e3
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2020-12-04 16:44:02 +0100

    Kártevőirtás: A regisztrációs űrlap feldolgozásához tartozó kód átírva (már jó)

--- Projekt/service_project/protected/services/pest_control/registration_db.php
+++ Projekt/service_project/protected/services/pest_control/registration_db.php
@@ -2,6 +2,10 @@
     require_once 'database.php';
 
     if(array_key_exists('registration', $_POST)){
+        registration();
+    }
+
+    function registration() {
         $firstName = $_POST['firstName'];
         $lastName = $_POST['lastName'];
         $email = $_POST['email'];
@@ -13,6 +17,11 @@
         $streetNumber = $_POST['streetNumber'];
         $floor = $_POST['floor'];
         $door = $_POST['door'];
+
+        if ($city == null || $city == "" || $postcode == null || $postcode == "" || $street == null || $street == "" || $streetNumber == null || $streetNumber == "" || $firstName == null || $firstName == "" || $lastName == null || $lastName == "" || $email == null || $email == "" || $password == null || $password == "" || $phone == null || $phone == "") {
+            echo "<p>Ellenőrizze, hogy minden szükséges adatott megadott-e!</p>";
+            return;
+        }
 
         $cityInsert = "INSERT INTO city(name, postcode) VALUES(:name, :postcode)";
         $cityParams = [
@@ -35,10 +44,11 @@
             ':city_id'  =>  $cityId
         ];
 
-        $addressId = getField("SELECT id FROM address WHERE street_name = :street_name AND street_number =: street_number AND floor = :floor AND door = :door", $addressParams);
+
+        $addressId = getField("SELECT id FROM address WHERE street_name = :street_name AND street_number = :street_number AND floor = :floor AND door = :door", [':street_name'  =>  $street, ':street_number' =>  $streetNumber, ':floor'    =>  $floor, ':door' =>  $door]);
         if($addressId == null) {
             executeDML($addressInsert, $addressParams);
-            $addressId = getField("SELECT id FROM address WHERE street_name = :street_name AND street_number =: street_number AND floor = :floor AND door = :door", $addressParams);
+            $addressId = getField("SELECT id FROM address WHERE street_name = :street_name AND street_number = :street_number AND floor = :floor AND door = :door", $addressParams);
         }
 
         $customerInsert = "INSERT INTO customer(first_name, last_name, email, phone, password, address_id) VALUES(:first_name, :last_name, :email, :phone, :password, :address_id)";
@@ -52,7 +62,7 @@
         ];
 
         $customerId = getField("SELECT id FROM customer WHERE email = :email", [':email' => $email]);
-        if(id == null && executeDML($customerInsert, $customerParams)) {
+        if($customerId == null && executeDML($customerInsert, $customerParams)) {
             echo "<p>Sikeres regisztráció.</p>";
         }
         else {
