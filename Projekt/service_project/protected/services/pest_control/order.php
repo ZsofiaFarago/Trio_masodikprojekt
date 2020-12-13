984f1f4b184d528bc8a72457413cdb494999df06
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2020-12-13 19:05:21 +0100

    Kártevőirtás: a megrendelés űrlap kiegészítése, módosítása

--- Projekt/service_project/protected/services/pest_control/order.php
+++ Projekt/service_project/protected/services/pest_control/order.php
@@ -5,8 +5,10 @@
 <p>Adja meg, melyik címre kéri a kártevőirtást! Ha ez megegyezik a regisztrációkor megadott címével, hagyja üresen!</p>
 <p>Végül válassza ki, milyen kártevő okozza a problémát!</p>
 
+<?php require_once 'order_db.php' ?>
+
 <div  class="form">
-    <form id="contactform">
+    <form id="contactform" method="POST">
     	<p class="contact"><label for="email">Email</label></p>
     	<input id="email" name="email" placeholder="example@email.com" type="email">
 
@@ -25,19 +27,47 @@
     	<p class="contact"><label for="streetNumber">Házszám</label></p>
     	<input id="streetNumber" name="streetNumber" placeholder="13" type="text">
 
-    	<p class="contact"><label for="floor">Emelet</label></p>
+    	<p class="contact"><label for="floor">Emelet (nem kötelező)</label></p>
     	<input id="floor" name="floor" placeholder="2" type="text">
 
-    	<p class="contact"><label for="door">Ajtó</label></p>
+    	<p class="contact"><label for="door">Ajtó (nem kötelező)</label></p>
     	<input id="door" name="door" placeholder="3" type="text">
 
         <p class="contact"><label for="pest">Kártevő típusa:</label></p>
-        <select name="pest" id="cars">
-            <option value="csotany">Csótány</option>
+        <select name="pest" id="pest">
+            <option value="csótány">Csótány</option>
             <option value="hangya">Hangya</option>
-            <option value="eger">Egér</option>
-            <option value="patkany">Patkány</option>
+            <option value="egér">Egér</option>
+            <option value="patkány">Patkány</option>
         </select> <br/>
-        <input class="buttom" name="order" id="submit" value="Megrendelés" type="submit">	 
+        <p class="contact"><label for="date">Válasszon egy napot egy hónapos időintervallumon belül, és hamarosan felvesszük Önnel a kapcsolatot telefonon időpontegyeztetéssel kapcsolatban!<br/>Aktuális időszak: <?= date("Y.m.d").' - '.date("Y.m.d", strtotime("+1 Months")).'.'?></label></p>
+        <select name="day" id="day">
+            <?php
+                $numberOfDays = cal_days_in_month(CAL_GREGORIAN, date("m"), date("Y"));
+                for ($i = date("d"); $i <= $numberOfDays; $i++) {
+                    $day = $i;
+                    if ($i < 10) {
+                        $day = "0".$i;
+                    }
+                    echo '<option value="', date('Y'), '-', date('m'), '-', $day, '">', date('Y'), '-', date('m'), '-', $day, '</option>';
+                }
+                $numberOfDays = cal_days_in_month(CAL_GREGORIAN, date("m", strtotime("+1 Months")), date("Y"));
+                for ($i = 1; $i <= date("d"); $i++) {
+                    $day = $i;
+                    if ($i < 10) {
+                        $day = "0".$i;
+                    }
+                    echo '<option value="', date('Y'), '-', date('m', strtotime("+1 Months")), '-', $day, '">', date('Y'), '-', date('m', strtotime("+1 Months")), '-', $day, '</option>';
+                }
+            ?>
+        </select><br/>
+        <p class="contact"><label for="time">Válasszon egy kívánt kezdési időt: </label></p>
+        <select name="time" id="time">
+            <option value="08:00:00">08:00</option>
+            <option value="10:00:00">10:00</option>
+            <option value="14:00:00">14:00</option>
+            <option value="16:00:00">16:00</option>
+        </select><br/>
+        <input class="buttom" name="order" id="submit" value="Megrendelés" type="submit">
    </form> 
 </div>
